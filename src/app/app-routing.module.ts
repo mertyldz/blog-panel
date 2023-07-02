07055c066a218702dd74200ec718e76e61b9530f
Mert Yıldız <yzmert3@gmail.com>
2023-07-02 14:39:08 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './pages/user/user.component';
+import { PostComponent } from './pages/post/post.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { CommentComponent } from './pages/comment/comment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'comment', component: CommentComponent },
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
