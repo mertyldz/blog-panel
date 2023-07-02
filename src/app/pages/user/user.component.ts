357ee18879f2716ddc1136816f5db1231c161ba9
Mert Yıldız <yzmert3@gmail.com>
2023-07-02 14:41:38 +0300

    create page components

---
+++ src/app/pages/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+
+  constructor(private userService:UserService){}
+
+  users:User[] = this.userService.getUserList();
+  
+}
