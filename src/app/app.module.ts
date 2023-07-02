1d37f7623a64070bbe17f5b77a8dbe99779bb68f
Mert Yıldız <yzmert3@gmail.com>
2023-07-02 14:39:51 +0300

    import components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './commons/navbar/navbar.component';
+import { UserComponent } from './pages/user/user.component';
+import { PostComponent } from './pages/post/post.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { CommentComponent } from './pages/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserComponent,
+    PostComponent,
+    CategoryComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
