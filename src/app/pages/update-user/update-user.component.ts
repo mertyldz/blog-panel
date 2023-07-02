79005ce782c970ad20bfe0e3e7cf5744480c5005
Mert Yıldız <yzmert3@gmail.com>
2023-07-02 22:53:45 +0300

    add update user page

--- src/app/pages/update-user/update-user.component.ts
+++ src/app/pages/update-user/update-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { User } from '../user/user';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-user',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-user.component.css']
 })
 export class UpdateUserComponent {
+  updateUserForm:FormGroup;
+  constructor(fb:FormBuilder, private userService:UserService, private activatedRoute: ActivatedRoute,private router:Router){ 
+    this.updateUserForm=fb.group({
+      username: '',
+      email: ''
+    })
+  }
+
+
+  updateUser(){
+    let id = this.activatedRoute.snapshot.paramMap.get("id");
+    let activeId:number = Number(id);
+    this.userService.updateUser(activeId , this.updateUserForm.value.username, this.updateUserForm.value.email);
+    alert("Başarıyla güncellendi.")
+    this.router.navigateByUrl('/user')
+    
+  }
+
+ 
+
+
+
+  
 
 }
