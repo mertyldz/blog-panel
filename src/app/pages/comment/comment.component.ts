9bbb1fb320382169532b6ab094eb13ae5eb48f48
Mert Yıldız <yzmert3@gmail.com>
2023-07-03 19:49:44 +0300

    add remove func

--- src/app/pages/comment/comment.component.ts
+++ src/app/pages/comment/comment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommentService } from 'src/app/services/comment.service';
+import { Comment } from './comment';
 
 @Component({
   selector: 'app-comment',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./comment.component.css']
 })
 export class CommentComponent {
+  comments:Comment[];
+
+  constructor(private commentService:CommentService){
+    this.comments=this.commentService.getComments();
+  }
+
+  removeComment(id:number){this.commentService.removeComment(id)}
 
 }
