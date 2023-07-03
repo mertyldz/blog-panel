78b531f89f1fab81235f8f59418e35efee7d1b69
Mert Yıldız <yzmert3@gmail.com>
2023-07-03 21:58:50 +0300

    add add-comment

--- src/app/pages/comment/add-comment/add-comment.component.ts
+++ src/app/pages/comment/add-comment/add-comment.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Comment } from '../comment';
+import { CommentService } from 'src/app/services/comment.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from '../../user/user';
 
 @Component({
   selector: 'app-add-comment',
   templateUrl: './add-comment.component.html',
-  styleUrls: ['./add-comment.component.css']
+  styleUrls: ['./add-comment.component.css'],
 })
 export class AddCommentComponent {
+  addCommentForm: FormGroup;
+  addNewComment: Comment;
+  userList:User[]=this.userService.getUserList();
 
+
+  confirmMenu: Confirmation[] = [
+    { status: 'Onaylandı', value: true },
+    { status: 'Onaylanmadı', value: false }
+  ];
+
+  constructor(fb: FormBuilder, private commentService: CommentService, private userService:UserService) {
+    this.addCommentForm = fb.group({
+      commentId: 0,
+      postId: 0,
+      userId: 1,
+      comment: '',
+      creationDate: '',
+      isConfirmed: false,
+    });
+    this.addNewComment = this.addCommentForm.value;
+  }
+
+  addComment() {
+    this.addNewComment = this.addCommentForm.value;
+    this.commentService.addComment(this.addNewComment);
+  }
+}
+
+export interface Confirmation {
+  status: string;
+  value: boolean;
 }
