d756a5ef14fdfdc042f17173149575310c588d41
Mert Yıldız <yzmert3@gmail.com>
2023-07-04 22:38:43 +0300

    add routing and filtering to category details

--- src/app/pages/category/detail-category/detail-category.component.ts
+++ src/app/pages/category/detail-category/detail-category.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 import { Category } from '../category';
 import { PostService } from 'src/app/services/post.service';
@@ -7,20 +7,30 @@ import { PostService } from 'src/app/services/post.service';
 @Component({
   selector: 'app-detail-category',
   templateUrl: './detail-category.component.html',
-  styleUrls: ['./detail-category.component.css']
+  styleUrls: ['./detail-category.component.css'],
 })
 export class DetailCategoryComponent {
-  selectedId:number;
-  selectedCategory:Category;
-  totalPost:number;
+  selectedId: number;
+  selectedCategory: Category;
+  totalPost: number;
 
-  constructor(private activatedRoute:ActivatedRoute, private categoryService:CategoryService, private postService:PostService){
-    this.selectedId=Number(activatedRoute.snapshot.paramMap.get("id"))
-    console.log(this.selectedId)
-    this.selectedCategory=this.categoryService.getCategoryById(this.selectedId);
-    this.totalPost=this.postService.getTotalPostByCategoryId(this.selectedId);
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private categoryService: CategoryService,
+    private postService: PostService,
+    private router: Router
+  ) {
+    this.selectedId = Number(activatedRoute.snapshot.paramMap.get('id'));
+    console.log(this.selectedId);
+    this.selectedCategory = this.categoryService.getCategoryById(
+      this.selectedId
+    );
+    this.totalPost = this.postService.getTotalPostByCategoryId(this.selectedId);
   }
 
-  
-
+  navigateToPosts() {
+    this.router.navigate(['/post'], {
+      queryParams: { categoryId: this.selectedId },
+    });
+  }
 }
