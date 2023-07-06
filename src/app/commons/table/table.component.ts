5f62eab2947ac0f85a6dd6c346ce01ef3737a895
Mert Yıldız <yzmert3@gmail.com>
2023-07-06 17:33:16 +0300

    add reusable table

---
+++ src/app/commons/table/table.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit{
+  @Input() titleList:string[]=[];
+  @Input() gridList:any[]=[];
+  @Input() notUser:boolean=true;
+  @Output() onDetail = new EventEmitter<any>();
+  @Output() onEdit = new EventEmitter<any>();
+  @Output() onDelete = new EventEmitter<any>();
+
+  pageNumber:number=10;
+
+
+  ngOnInit(): void {
+      
+  }
+
+  edit(item:any){
+    this.onEdit.emit(item);
+  }
+
+  detail(item:any){
+    this.onDetail.emit(item)
+  }
+
+  delete(item:any){
+    this.onDelete.emit(item);
+  }
+
+}
