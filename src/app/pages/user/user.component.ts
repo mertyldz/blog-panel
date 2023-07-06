43f188cca983b42dc4f16b871dc01cc43aea9791
Mert Yıldız <yzmert3@gmail.com>
2023-07-06 17:43:55 +0300

    fix user with re-table

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,15 +9,27 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit{
+  titleList:string[] = ['userId', 'username', 'email', 'creationDate']
+
+
+
   pageNumber:number=1;
   users:User[]=[]
-  constructor(private userService:UserService){}
+  constructor(private userService:UserService, private router:Router){}
   
   ngOnInit(): void {
     this.userService.getUserList().subscribe(x=>this.users=x);
 }
 
+  editUser(item:any){
+    this.userService.deleteUser(item.userId);
+  }
+
+  deleteUser(item:any){
+    this.router.navigate(['/user/' + item.userId]);
+  }
+
   removeUser(id:number){this.userService.deleteUser(id)}
   updateUser(id:number){console.log(`${id} + " güncellendi!`)}
-  
+   
 }
